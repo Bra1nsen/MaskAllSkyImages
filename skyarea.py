aeca93cce8d42350e32b14df9d609efa33bb6642
Diplomat <paulmatteschk@googlemail.com>
2022-12-27 14:51:24 +0100

    Update skyarea.py

--- skyarea.py
+++ skyarea.py
@@ -1,4 +1,8 @@
  ## Fisheye All Sky Images ##
+ 
+#1 original1 with shape = (1014,760) sky_circle1: -r 393 -c 574 335
+
+#2 original2 with shape = (2032,1520) sky_circle2: -r 1070 -c 1024 804
 ----------------------
 Idea:  Only Calculate with Sky Area Pixels (Solarpoweranalysis)
 
